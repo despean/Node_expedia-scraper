43d8de2b9dca20690240990f4a04367fe20449d1
despean <despean15@gmail.com>
2018-08-31 23:29:11 -0400

    fr scraper

--- routes/index.js
+++ routes/index.js
@@ -38,11 +38,28 @@ router.get('/uk', async function(req, res, next) {
         'locn='+encodeURI(params[0].split('=')[1])+'&retrieveNeighborhoods=true&clientTimeZoneOffset=0'
 
     // var url ='https://www.expedia.co.uk/carsearch/pickup/list/results/script?loc2=London%2C%20England%2C%20UK%20%28LCY-London%20City%29&time1=1000AM&time2=1000AM&retrieveUrgencyCount=true&ageInRange=true&date2=13%2F09%2F2018&date1=05%2F09%2F2018&locn=London%2C%20England%2C%20UK%20%28LHR-Heathrow%29&retrieveNeighborhoods=true&'
+    var val = "No data";
+    await scraper.get(url).then(data => {val = scraper.xml_gen(scraper.parser(JSON.parse(data)))})
+    res.contentType('application/xml')
+    res.send(val);
+});
+// var url ='https://www.expedia.fr/carsearch/pickup/list/results?pickUpDate='+ encodeURI(params[1].split('=')[1])+'&pickUpTime='+time+'&dropOffDate='
+//     +encodeURI(params[2].split('=')[1])+'&dropOffTime='+time+'&pickUpSearchType=4&pickUpSearchTerm='+encodeURI(params[0].split('=')[1])+'&dropOffSearchTerm='
+//     +encodeURI(params[0].split('=')[1])+'&dropOffSearchType=4&radiusDistance=25&age=30&ageInRange=true&clientTimeZoneOffset=0'
+
+router.get('/fr', async function(req, res, next) {
+    var params = req._parsedUrl.query.split('&')
+    var time = '1000AM';
+    if(params.length > 4){
+        time = encodeURI(params[4].split('=')[1])
+    }
+    var url ='https://www.expedia.fr/carsearch/pickup/list/results?pickUpDate='+ encodeURI(params[1].split('=')[1])+'&pickUpTime='+time+'&dropOffDate='
+    +encodeURI(params[2].split('=')[1])+'&dropOffTime='+time+'&pickUpSearchType=4&pickUpSearchTerm='+encodeURI(params[0].split('=')[1])+'&dropOffSearchTerm='
+    +encodeURI(params[0].split('=')[1])+'&dropOffSearchType=4&radiusDistance=25&age=30&ageInRange=true&clientTimeZoneOffset=0'
     console.log(url)
     var val = "No data";
     await scraper.get(url).then(data => {val = scraper.xml_gen(scraper.parser(JSON.parse(data)))})
     res.contentType('application/xml')
     res.send(val);
 });
-
 module.exports = router;
